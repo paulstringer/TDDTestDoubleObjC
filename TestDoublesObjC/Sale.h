c842cfaeecbcad23acfbd4c9b5448e4947519e2f
Paul Stringer <paulstringer@mac.com>
2016-05-19 12:03:18 +0100

    Adding new project and Sale class

---
+++ TestDoublesObjC/Sale.h
@@ -0,0 +1,15 @@
+//
+//  Sale.h
+//  TestDoublesObjC
+//
+//  Created by Paul Stringer on 19/05/2016.
+//  Copyright © 2016 stringerstheory. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Sale : NSObject
+
+- (void) scan:(NSString *)barcode;
+
+@end
---
+++ TestDoublesObjC/Sale.m
@@ -0,0 +1,18 @@
+//
+//  Sale.m
+//  TestDoublesObjC
+//
+//  Created by Paul Stringer on 19/05/2016.
+//  Copyright © 2016 stringerstheory. All rights reserved.
+//
+
+#import "Sale.h"
+
+@implementation Sale
+
+- (void) scan:(NSString *)barcode {
+    // TODO: Print the Price and Item Name on a Display
+    
+}
+
+@end
